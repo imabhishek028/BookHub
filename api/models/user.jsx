55ee7c18834e8f50f27e91e4df3939ee2b321c03
imabhishek028 <imabhishek028@gmail.com>
2024-07-20 02:01:06 +0530

    Bug Fix -1

--- api/models/user.jsx
+++ api/models/user.jsx
@@ -37,6 +37,10 @@ const userSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    photo:{
+        type:String,
+        default:null
     }
 })
 
