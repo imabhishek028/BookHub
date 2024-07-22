aa9005623b791899e2e3647be47ac5090de5a286
imabhishek028 <imabhishek028@gmail.com>
2024-07-22 23:50:53 +0530

    focus effect on myboooks screens

--- screens/MyBooks.jsx
+++ screens/MyBooks.jsx
@@ -1,22 +1,22 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { FlatList, ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
-import { scale } from 'react-native-size-matters'
-import axiosInstance from '../assets/utils/axiosConfig'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { SafeAreaView, StatusBar, StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { FlatList, ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { scale } from 'react-native-size-matters';
+import axiosInstance from '../assets/utils/axiosConfig';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { useFocusEffect } from '@react-navigation/native';
 
 const MyBooks = ({ navigation }) => {
     const [email, setUserEmail] = useState('');
     const [data, setData] = useState([]);
-    const [loading,setLoading]=useState(false);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const getUserEmail = async () => {
             try {
                 const email = await AsyncStorage.getItem('userEmail');
-                if(email)
-                setUserEmail(email);
+                if (email) setUserEmail(email);
             } catch (err) {
                 console.log(err);
             }
@@ -24,61 +24,64 @@ const MyBooks = ({ navigation }) => {
         getUserEmail();
     }, []);
 
-    useEffect(() => {
-        const getCollections = async () => {
-            setLoading(true);
-            try {
-                const response = await axiosInstance('/getCurrentCollections', {
-                    params: { email: email }
-                });
-                console.log(response.data);
-                setData(response.data);
-            } catch (err) {
-                console.log(err);
-            }
-            finally{
-                setLoading(false)
-            }
-        };
-
-        if (email) {
-            getCollections();
+    const getCollections = async () => {
+        setLoading(true);
+        try {
+            const response = await axiosInstance('/getCurrentCollections', {
+                params: { email: email }
+            });
+            console.log(response.data);
+            setData(response.data);
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
         }
-    }, [email]);
+    };
 
-    const onPressHandler=(item)=>{
-        navigation.navigate('CreatedBookView',{item, email})
-    }
+    useFocusEffect(
+        useCallback(() => {
+            if (email) {
+                getCollections();
+            }
+        }, [email])
+    );
+
+    const onPressHandler = (item) => {
+        navigation.navigate('CreatedBookView', { item, email });
+    };
 
     const renderItem = ({ item }) => {
         const defaultImage = 'https://via.placeholder.com/100x160.png?text=No+Image';
         return (
-          <TouchableOpacity 
-          style={styles.renderItem}
-          onPress={()=>onPressHandler(item)}>
-            <View style={{ flexDirection: 'row', gap: scale(20) }}>
-              <Image
-                source={{ uri: item.coverImage || defaultImage }}
-                style={styles.coverImages}
-                resizeMode='contain'
-              />
-              <View style={{ flex: 1, width:scale(50) }}>
-                <View style={styles.bookTitleView}>
-                  <Text style={styles.bookTitle}>{item.title}</Text>
+            <TouchableOpacity 
+                style={styles.renderItem}
+                onPress={() => onPressHandler(item)}
+            >
+                <View style={{ flexDirection: 'row', gap: scale(20) }}>
+                    <Image
+                        source={{ uri: item.coverImage || defaultImage }}
+                        style={styles.coverImages}
+                        resizeMode='contain'
+                    />
+                    <View style={{ flex: 1, width: scale(50) }}>
+                        <View style={styles.bookTitleView}>
+                            <Text style={styles.bookTitle}>{item.title}</Text>
+                        </View>
+                        <View style={styles.authorView}>
+                            <Text style={{ color: 'gray', fontSize: scale(14) }}>by</Text>
+                            <Text style={styles.bookAuthor}>{item.author}</Text>
+                        </View>
+                        <View style={styles.publishYearView}>
+                            <Text style={styles.publishYearText}>Genre:</Text>
+                            <Text style={styles.yearText}> {item.genre || 'N/A'}</Text>
+                        </View>
+                    </View>
                 </View>
-                <View style={styles.authorView}>
-                  <Text style={{ color: 'gray', fontSize: scale(14) }}>by</Text>
-                  <Text style={styles.bookAuthor}>{item.author}</Text>
-                </View>
-                <View style={styles.publishYearView}>
-                  <Text style={styles.publishYearText}>Genre:</Text>
-                  <Text style={styles.yearText}> {item.genre || 'N/A'}</Text>
-                </View>
-              </View>
-            </View>
-          </TouchableOpacity>
+            </TouchableOpacity>
         );
-      }
+    };
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={{ flexDirection: 'column' }}>
@@ -103,7 +106,8 @@ const MyBooks = ({ navigation }) => {
                         <TouchableOpacity
                             onPress={() => {
                                 navigation.navigate('CreateBook')
-                            }}>
+                            }}
+                        >
                             <Text style={styles.touchableText}>
                                 Create Book
                             </Text>
@@ -124,18 +128,22 @@ const MyBooks = ({ navigation }) => {
                         My Collections!
                     </Text>
                 </View>
-                <FlatList
+                {loading ? (
+                    <ActivityIndicator size="large" color="#041E42" />
+                ) : (
+                    <FlatList
                         data={data}
                         renderItem={renderItem}
                         keyExtractor={(item) => item._id.toString()}
-                        style={{marginBottom:scale(200)}}
+                        style={{ marginBottom: scale(200) }}
                     />
+                )}
             </View>
         </SafeAreaView>
-    )
-}
+    );
+};
 
-export default MyBooks
+export default MyBooks;
 
 const styles = StyleSheet.create({
     container: {
@@ -156,64 +164,64 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         padding: scale(15),
         fontSize: scale(15),
-        fontWeight: 'bold'
+        fontWeight: 'bold',
     },
     favText: {
         color: '#041E42',
         fontWeight: 'bold',
         fontSize: scale(24),
         textDecorationLine: 'underline',
-        marginBottom:scale(10)
+        marginBottom: scale(10),
     },
     favTextView: {
         alignContent: 'flex-start',
         marginTop: scale(20),
-        marginLeft: scale(20)
+        marginLeft: scale(20),
     },
-  renderItem: {
-    marginLeft: scale(10),
-    marginRight: scale(10),
-    marginTop: scale(10),
-    backgroundColor: '#F8F8F8',
-    padding: scale(10),
-    borderRadius: scale(5),
-    elevation: 1,
-  },
-  bookTitle: {
-    fontSize: scale(16),
-    fontWeight: 'bold',
-    color: '#041E42',
-    flexShrink: 1,
-  },
-  bookAuthor: {
-    fontSize: scale(14),
-    color: '#041E42',
-    marginLeft: scale(5),
-  },
-  bookTitleView: {
-    marginBottom: scale(5),
-  },
-  authorView: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: scale(5),
-    width:scale(160)
-  },
-  publishYearView: {
-    marginTop: scale(5),
-    flexDirection:'row'
-  },
-  publishYearText: {
-    fontSize: scale(12),
-    color: 'gray',
-  },
-  yearText:{
-    fontSize: scale(12),
-    color: '#041E42',
-  },
-  coverImages: {
-    height: scale(100),
-    width: scale(100),
-    marginLeft: scale(10), 
-  },
-})
+    renderItem: {
+        marginLeft: scale(10),
+        marginRight: scale(10),
+        marginTop: scale(10),
+        backgroundColor: '#F8F8F8',
+        padding: scale(10),
+        borderRadius: scale(5),
+        elevation: 1,
+    },
+    bookTitle: {
+        fontSize: scale(16),
+        fontWeight: 'bold',
+        color: '#041E42',
+        flexShrink: 1,
+    },
+    bookAuthor: {
+        fontSize: scale(14),
+        color: '#041E42',
+        marginLeft: scale(5),
+    },
+    bookTitleView: {
+        marginBottom: scale(5),
+    },
+    authorView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: scale(5),
+        width: scale(160),
+    },
+    publishYearView: {
+        marginTop: scale(5),
+        flexDirection: 'row',
+    },
+    publishYearText: {
+        fontSize: scale(12),
+        color: 'gray',
+    },
+    yearText: {
+        fontSize: scale(12),
+        color: '#041E42',
+    },
+    coverImages: {
+        height: scale(100),
+        width: scale(100),
+        marginLeft: scale(10),
+    },
+});
